fc6c4b6135e49fde3df778b16074734c1db1032d
Jason Brant <jason.brant@blackboard.com>
2019-09-10 18:50:22 -0400

    Adding gradient

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+import numpy as np
 
 ########### Pick variables
 tabtitle='dog breeds'
@@ -11,6 +13,68 @@ x_list=['Australian Cattle Dogs', 'French Bulldogs', 'Corgis', 'Pugs']
 y_list=[12, 4, 6, 5]
 mytitle='My favorite dog breeds'
 githublink='https://github.com/calijason76/zoo-animals-dash'
+
+np.random.seed(19680801)
+
+
+def gradient_image(ax, extent, direction=0.3, cmap_range=(0, 1), **kwargs):
+    """
+    Draw a gradient image based on a colormap.
+
+    Parameters
+    ----------
+    ax : Axes
+        The axes to draw on.
+    extent
+        The extent of the image as (xmin, xmax, ymin, ymax).
+        By default, this is in Axes coordinates but may be
+        changed using the *transform* kwarg.
+    direction : float
+        The direction of the gradient. This is a number in
+        range 0 (=vertical) to 1 (=horizontal).
+    cmap_range : float, float
+        The fraction (cmin, cmax) of the colormap that should be
+        used for the gradient, where the complete colormap is (0, 1).
+    **kwargs
+        Other parameters are passed on to `.Axes.imshow()`.
+        In particular useful is *cmap*.
+    """
+    phi = direction * np.pi / 2
+    v = np.array([np.cos(phi), np.sin(phi)])
+    X = np.array([[v @ [1, 0], v @ [1, 1]],
+                  [v @ [0, 0], v @ [0, 1]]])
+    a, b = cmap_range
+    X = a + (b - a) / X.max() * X
+    im = ax.imshow(X, extent=extent, interpolation='bicubic',
+                   vmin=0, vmax=1, **kwargs)
+    return im
+
+
+def gradient_bar(ax, x, y, width=0.5, bottom=0):
+    for left, top in zip(x, y):
+        right = left + width
+        gradient_image(ax, extent=(left, right, bottom, top),
+                       cmap=plt.cm.Blues_r, cmap_range=(0, 0.8))
+
+
+xmin, xmax = xlim = 0, 10
+ymin, ymax = ylim = 0, 1
+
+fig, ax = plt.subplots()
+ax.set(xlim=xlim, ylim=ylim, autoscale_on=False)
+
+# background image
+gradient_image(ax, direction=0, extent=(0, 1, 0, 1), transform=ax.transAxes,
+               cmap=plt.cm.Oranges, cmap_range=(0.1, 0.6))
+
+N = 10
+x = np.arange(N) + 0.15
+y = np.random.rand(N)
+gradient_bar(ax, x, y, width=0.7)
+ax.set_aspect('auto')
+plt.show()
+
+
 
 ########### Set up the chart
 mydata = [go.Bar(x=x_list,
